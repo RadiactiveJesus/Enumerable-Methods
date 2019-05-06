394caf4c8e9b68d494b42a564b07020e056bef50
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-05-06 05:28:06 -0500

    adding feature for evaluating hashes

--- my_Enumerable.rb
+++ my_Enumerable.rb
@@ -1,70 +1,148 @@
 module Enumerable
     def my_each
-        for i in (0..self.length - 1) do
-          yield(self[i])
+        if self.class == Array
+            for i in (0..self.length - 1) do
+            yield(self[i])
+            end 
+        elsif self.class == Hash
+            for i in (0..self.length - 1) do
+                yield(self.keys[i], self.value[i])
+            end
         end 
     end
     def my_each_with_index
-        for i in (0..self.length - 1) do
-            yield(self[i], i)
+        if self.class == Array
+            for i in (0..self.length - 1) do
+                yield(self[i], i)
+            end
+        elsif self.class == Hash
+            for i in (0..self.keys.length - 1) do
+                yield(self.keys[i], self.values[i], i)
+            end
         end
     end
     def my_select
-        result_array = []
-        self.my_each do |value|
-          if yield(value)
-            result_array.push(value)
-          end
-        end
-        return result_array
+        if self.class == Array
+            result_array = []
+            self.my_each do |value|
+                if yield(value)
+                    result_array.push(value)
+                end
+            end
+            return result_array
+        elsif self.class == Hash
+            result_hash = {}
+            self.my_each do |key, value|
+              if yield(key, value)
+                result_hash[key] = value
+              end
+            end
+            return result_hash
+        end 
     end
     def my_all?
-        self.my_each do |value|
-            if !yield(value)
-              return false
+        if self.class == Array
+            self.my_each do |value|
+                if !yield(value)
+                return false
+                end
+            end
+        elsif self.class == Hash
+            self.my_each do |key, value|
+              if !yield(key, value)
+                return false
+              end
             end
         end
+        return true
     end
     def my_any?
-        self.my_each do |value|
-            if yield(value)
-              return true
+        if self.class == Array
+            self.my_each do |value|
+                if yield(value)
+                return true
+                end
             end
+        elsif self.class == Hash
+            self.my_each do |key, value|
+                if yield(key, value)
+                  return true
+                end
+            end
+            false
         end
     end
     def my_none?
-        self.my_each do |value|
-            if yield(value)
-              return false
+        if self.class == Array
+            self.my_each do |value|
+                if yield(value)
+                return false
+                end
             end
+        elsif self.class == Hash
+            self.my_each do |key, value|
+                if yield(key, value)
+                  return false
+                end
+            end
+            false
         end
+        true
     end
     def my_count
         count = 0
-        self.my_each do |value|
-        if yield(value)
-          count += 1
+        if self.class == Array
+            self.my_each do |value|
+                if yield(value)
+                count += 1
+                end
+            end
+        elsif self.class == Hash
+            self.my_each do |key, value|
+                if yield(key, value)
+                  count += 1
+                end
+            end
         end
-      end
+        return count
     end
     def my_map(proc = '')
-        result_array = []
-        if proc.is_a? (Proc)
-            self.my_each_with_index do |value, i|
-            result_array[i] = proc.call(value)
+        if self.class == Array
+            result_array = []
+            if proc.is_a? (Proc)
+                self.my_each_with_index do |value, i|
+                result_array[i] = proc.call(value)
+                end
+            else
+                self.my_each_with_index do |value, i|
+                result_array[i] = yield(value)
+                end
             end
-        else
-            self.my_each_with_index do |value, i|
-            result_array[i] = yield(value)
+            return result_array
+        elsif self.class == Hash
+            result_hash = {}
+            if proc.is_a? (Proc)
+                self.my_each do |key, value|
+                    result_hash[key] = proc.call(key, value)
+                end
+            else
+                self.my_each do |key, value|
+                    result_hash[key] = yield(key, value)
+                end
             end
+            return result_hash
         end
-        return result_array
     end
     def my_inject(x)
         output = x
-        self.my_each do |value|
-            total = yield(total, value)
+        if self.class == Array
+            self.my_each do |value|
+                total = yield(total, value)
+            end
+        elsif self.class == Hash
+            self.my_each do |key, value|
+              total = yield(total, value)
         end
-        return output
+        return total
     end
 end
