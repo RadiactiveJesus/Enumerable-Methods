9e89587c951794c2c71a46aca4c37e3408407859
Rodolfo Llinás <rllinasc@unal.edu.co>
2019-05-09 06:13:39 -0500

    fixing bugs

--- my_Enumerable.rb
+++ my_Enumerable.rb
@@ -1,14 +1,15 @@
 module Enumerable
     def my_each
         if self.class == Array
-            for i in (0..self.length) do
+            for i in (1..self.length) do
                 yield(self[i])
             end 
         elsif self.class == Hash
-            for i in (0..self.length) do
+            for i in (1..self.length) do
                 yield(self.keys[i], self.value[i])
             end
         end 
+        return self
     end
     def my_each_with_index
         if self.class == Array
@@ -89,49 +90,33 @@ module Enumerable
         end
         true
     end
-    def my_count(item, hash_value)
+    def my_count
         count = 0
-        if self.class == Array
-            self.my_each do |value|
-                if item == value
-                    count += 1
+        if block_given?
+            for i in 0..self.length - 1
+                if yield self[i]
+                  count += 1
                 end
             end
-        elsif self.class == Hash
-            self.my_each do |key, value|
-                if item == key && hash_value == value
-                    count += 1
-                end
-            end
+        else
+            self.length
         end
         return count
     end
-    def my_map(proc = '')
-        if self.class == Array
-            result_array = []
-            if proc.is_a? (Proc)
-                self.my_each_with_index do |value, i|
-                    result_array[i] = proc.call(value)
-                end
-            else
-                self.my_each_with_index do |value, i|
-                    result_array[i] = yield(value)
-                end
-            end
-            return result_array
-        elsif self.class == Hash
-            result_hash = {}
-            if proc.is_a? (Proc)
-                self.my_each do |key, value|
-                    result_hash[key] = proc.call(key, value)
-                end
-            else
-                self.my_each do |key, value|
-                    result_hash[key] = yield(key, value)
-                end
-            end
-            return result_hash
+    def my_map (p = nil)
+        item = []
+        i = 0
+        while i < self.length
+          if not block_given?
+            result = p.call
+            item.push(result)
+          else
+            result = yield self[i]
+            item.push(result)
+          end
+          i = i + 1
         end
+        item
     end
     def my_inject(x)
         output = x
@@ -147,3 +132,5 @@ module Enumerable
         return total
     end
 end
+array= [1, 2, 3, 4, 5]
+print array.my_map {|i| i + 1}
